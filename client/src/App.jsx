9af1ade50d901fbb772562673c70038b5ede7f11
OreaRoros <orearrs@gmail.com>
2024-01-12 18:27:40 -0800

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import Signin from "./pages/Signin"
-import SignUp from "./pages/SignUp"
-import Profile from "./pages/Profile"
-import About from "./pages/About"
-import Navbar from "./components/Navbar"
-import './index.css'
+import React from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/sign-in" exact element={<Signin />} />
-        <Route path="/sign-up" exact element={<SignUp />} />
-        <Route path="/profile" exact element={<Profile />} />
-        <Route path="/about" exact element={<About />} />
+        <Route path='/' exact element={<Home />} />
+        <Route path='/sign-in' exact element={<SignIn />} />
+        <Route path='/sign-up' exact element={<SignUp />} />
+        <Route path='/about' exact element={<About />} />
+        <Route path='/profile' exact element={<Profile />} />
       </Routes>
     </BrowserRouter>
   )
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { FaSearch } from "react-icons/fa";
+import {FaSearch} from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
@@ -13,24 +12,24 @@ export default function Navbar() {
                 </h1>
             </Link>
 
-            <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-                <input 
+            <form className="bg-slate-100 rounded-lg flex items-center p-3">
+                <input
                     type="text" 
-                    placeholder='search...' 
-                    className='bg-transparent focus:outline-none w-24 sm:w-64'
+                    placeholder='Search' 
+                    className="bg-transparent focus:outline-none w-24 sm:w-64"
                 />
-                <FaSearch className='text-slate-600 cursor-pointer' />
+                <FaSearch className="text-slate-500" />
             </form>
 
             <ul className='flex gap-4'>
                 <Link to="/">
-                    <li className='hidden sm:inline text-slate-700 hover:text-slate-500'>Home</li>
+                    <li className="hidden sm:inline text-slate-700 hover:text-slate-500">Home</li>
                 </Link>
                 <Link to="/about">
-                    <li className='hidden sm:inline text-slate-700 hover:text-slate-500'>About</li>
+                    <li className="hidden sm:inline text-slate-700 hover:text-slate-500">About</li>
                 </Link>
                 <Link to="/sign-in">
-                    <li className='text-slate-700 hover:text-slate-500'>Sign-in</li>
+                    <li className="text-slate-700 hover:text-slate-500">Sign in</li>
                 </Link>
             </ul>
         </div>
